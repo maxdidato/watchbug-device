1747964e278adba135300815e97632c57b65ea71
mdidato <f>
2015-08-01 12:00:59 +0100

    including Geogra Library

---
+++ GeogramONE/examples/GO_V3_0_EEPROM_Program/eepromAnything.h
@@ -0,0 +1,26 @@
+#include <EEPROM.h>
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+template <class T> int EEPROM_writeAnything(int ee, const T& value)
+{
+    const char* p = (const char*)(const void*)&value;
+    unsigned int i;
+    for (i = 0; i < sizeof(value); i++)
+        EEPROM.write(ee++, *p++);
+    return i;
+}
+
+template <class T> int EEPROM_readAnything(int ee, T& value)
+{
+    char* p = (char*)(void*)&value;
+    unsigned int i;
+    for (i = 0; i < sizeof(value); i++)
+		*p++ = EEPROM.read(ee++);
+    return i;
+}
+
